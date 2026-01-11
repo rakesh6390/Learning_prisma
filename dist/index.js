759e73585ed5c066c5ae3ee7349b6c5f4b87b36d
rakesh6390 <it12212035@gmail.com>
2026-01-12 01:58:58 +0530

    first

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+const client = new PrismaClient();
+async function createUser() {
+    await client.user.create({
+        data: {
+            username: "yogesh rand",
+            password: "123456",
+            age: 35,
+            city: "delhi"
+        }
+    });
+}
+createUser();
+//# sourceMappingURL=index.js.map
